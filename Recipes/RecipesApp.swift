c0187c803ef58673f16dbb7ba28dfbf7bcca3bb6
FredericXS <xsfrederic@gmail.com>
2023-05-21 14:12:18 -0300

    Initial Commit

---
+++ Recipes/RecipesApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipesApp.swift
+//  Recipes
+//
+//  Created by Ashb0rn on 21/05/23.
+//
+
+import SwiftUI
+
+@main
+struct RecipesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
